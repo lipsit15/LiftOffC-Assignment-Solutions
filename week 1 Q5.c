0fe3dd1f158ff934f273eaefd6131ac485293d2c
lipsit15 <78718207+lipsit15@users.noreply.github.com>
2021-02-10 09:51:27 +0530

    Add files via upload

---
+++ week 1 Q5.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+int main()
+{
+    char ch;
+    printf("enter your alphabet");
+    scanf("%c",&ch);
+    switch(ch);
+    {
+        case 'a';
+        printf("IT IS A VOWEL");
+        break;
+        case 'e';
+        printf("IT IS A VOWEL");
+        break;
+        case 'i'
+        printf("IT IS A VOWEL");
+        break;
+        case 'o';
+        printf("IT IS A VOWEL");
+        break;
+        case 'u';
+        printf("IT IS A VOWEL");
+        break;
+        default;
+        printf("IT'S A CONSONANT");
+
+    }
+    return 0;
+     
+}
