2e1d63e0f5bb5ef2ab1cce075c1014c246cefbdf
lipsit15 <78718207+lipsit15@users.noreply.github.com>
2021-02-10 09:49:50 +0530

    Add files via upload

---
+++ week 1 Q 2.c
@@ -0,0 +1,12 @@
+
+#include<stdio.h>
+
+int main(){
+    float celsius = 0, fahrenheit;
+
+    fahrenheit = ( celsius * 1.8 ) + 32;
+
+    printf("%f Celsius = %f Fahrenheit",celsius,fahrenheit);
+
+    return 0;
+}
