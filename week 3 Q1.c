65c5bb9a812c69bbac70cb8eedc3189fab1b1268
lipsit15 <78718207+lipsit15@users.noreply.github.com>
2021-04-01 22:54:22 +0530

    Add files via upload

---
+++ week 3 Q1.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+int square(int n){
+    return n*n;
+}
+int main(){
+    printf("Enter a number:");
+    int n;
+    scanf("%d",&n);
+    printf("%d",square(n));
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
